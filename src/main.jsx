89cec956208542164bb394f414c097d5658d01ab
mategurrea02 <mategurrea@gmail.com>
2024-10-07 10:49:00 -0300

    agregando navegacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import Home from './pages/Home.jsx'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
+import Home from './pages/Home.jsx'
+import Login from './pages/login/Login.jsx'
+import Register from './pages/register/Register.jsx'
 
 AOS.init();
 
-createRoot(document.getElementById('root')).render(
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+]
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Home />
+    <Router>
+        <Routes>
+        {
+          routes.map(route => (
+            <Route
+            path={route.path}
+            element={route.element}
+            key={route.path}
+            />))
+        }
+      </Routes>
+    </Router>
   </StrictMode>,
 )
