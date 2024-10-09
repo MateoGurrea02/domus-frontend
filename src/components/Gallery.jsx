977216a2b8e9c4210b2752cf9146afdca1484900
mategurrea02 <mategurrea@gmail.com>
2024-10-09 17:05:33 -0300

    fix y agregando container

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,11 @@
 const Gallery = ({imgMain, imgListSecondary})=>{
   return(
     <>
-      <div className="flex justify-center m-5">
-        <div className='flex w-3/5'>
+      <div className="flex justify-center">
+        <div className='flex w-5/6'>
           <img className='rounded-l-2xl shadow-md' src={imgMain} alt="" />
         </div>
-        <div className='flex flex-col w-1/4 rounded-md'>
+        <div className='flex flex-col w-2/6 rounded-md'>
           <img className='rounded-tr-2xl ml-2 duration-200 hover:rounded-2xl shadow-md ' src={imgListSecondary[0]} alt="" />
           <img className='mt-2 ml-2 duration-200 hover:rounded-2xl shadow-md' src={imgListSecondary[1]} alt="" />
           <img className='rounded-br-2xl mt-2 ml-2 duration-200 hover:rounded-2xl shadow-md' src={imgListSecondary[2]} alt="" />
