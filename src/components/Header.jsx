dec1f58b319108c67286b46c078b21dc04fc73e5
mategurrea02 <mategurrea@gmail.com>
2024-10-24 18:37:18 -0300

    rutas en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,13 +37,19 @@ const Header = () => {
           <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
               <li>
-                <p href="" className="block py-2 px-3 md:p-0 text-gray-500 rounded md:bg-transparent md:hover:text-gray-900" aria-current="page">Inicio</p>
+                <Link to={'/'}>
+                  <p href="" className="block py-2 px-3 md:p-0 text-gray-500 rounded md:bg-transparent md:hover:text-gray-900" aria-current="page">Inicio</p>
+                </Link>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 md:p-0 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-900 ">Propiedades</a>
+                <Link to={"/property-search"}>
+                  <p className="block py-2 px-3 md:p-0 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-900 ">Propiedades</p>
+                </Link>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 md:p-0 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-900 ">Alquileres</a>
+                <Link to={"/rental-search"}>
+                  <p href="#" className="block py-2 px-3 md:p-0 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-900 ">Alquileres</p>
+                </Link>
               </li>
             </ul>
           </div>
